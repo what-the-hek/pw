2b006179cf5264bcd5e0706e95f028d111b53e12
what-the-hek <108735805+what-the-hek@users.noreply.github.com>
2024-08-04 19:17:54 +0800

    on change length and generate random password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,15 +13,40 @@ export class AppComponent {
   includeLetters = false;
   includeNumbers = false;
   includeSymbols = false;
+  length = 0;
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+    if(parsedValue==null){
+      return
+    }
+    else if(!isNaN(parsedValue)) {
+      this.length = parsedValue
+    }
+  }
 
   onButtonClick() {
-    this.password = 'My password';
-    console.log(`
-      About to generate a password with the following:
-      Includes letters: ${this.includeLetters}
-      Includes numbers: ${this.includeNumbers}
-      Includes symbols: ${this.includeSymbols}
-      `)
+    const numbers = '1234567890'
+    const letters = 'abcdefghijklmnopqrstuvwxyz'
+    const symbols = '!@#$%^&*()_+';
+    
+    let validChars = ''
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = ''
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword
   }
 
   onChangeUseLetters() {
